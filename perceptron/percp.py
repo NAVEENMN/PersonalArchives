a36de7e630c490eda7b350bbec38c80e36f0bac6
Naveen Mysore <navimn1991@gmail.com>
2016-12-04 14:46:05 -0800

    classification plot

--- perceptron/percp.py
+++ perceptron/percp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 import numpy as np
 
 class Perceptron(object):
@@ -35,6 +36,25 @@ class Perceptron(object):
 	def predict(self, X):
 		return np.where(self.net_input(X) >= 0.01, 1, -1)
 
+# This function plots and draws classification boundries
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+	#setup marker generators and color map
+	markers = ('s', 'x', 'o', '^', 'v')
+	colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+	cmap = ListedColormap(colors[:len(np.unique(y))])
+	#plot decision surface
+	x1_min, x1_max = X[:, 0].min() - 1, X[:,0].max() + 1
+	x2_min, x2_max = X[:, 1].min() - 1, X[:,1].max() + 1
+	xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+	Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+	Z = Z.reshape(xx1.shape)
+	plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+	plt.xlim(xx1.min(), xx1.max())
+	plt.ylim(xx2.min(), xx1.max())
+	#plot class samples
+	for idx, cl in enumerate(np.unique(y)):
+		plt.scatter(x=X[y==cl, 0], y=X[y==cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)  
+
 def main():
 	# ---- data process ----
 	df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None) # load date set of iris from UCI
@@ -56,6 +76,12 @@ def main():
 	plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
 	plt.xlabel('Epochs')
 	plt.ylabel('# of missclassifications')
+	plt.show()
+	# ---- plot decsion boundry on sample space
+	plot_decision_regions(X, y, ppn)
+	plt.xlabel('sepal length')
+	plt.ylabel('petal lenght')
+	plt.legend(loc='upper left')
 	plt.show()
 	
 
