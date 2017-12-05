08182a574df35eacb7ae4b6379fcd80700382ba2
Naveen Mysore <navimn1991@gmail.com>
2017-12-05 08:51:08 -0800

    <<

--- main.py
+++ main.py
@@ -2,99 +2,117 @@ import tensorflow as tf
 import util as ut
 import gym
 import numpy as np
+from collections import deque
 from PIL import Image
+import random
+from network import *
 
 IMAGE_HEIGHT = 160
 IMAGE_WIDTH = 160
 IMAGE_CHANNEL = 3
 IMAGE_PIXELS = IMAGE_HEIGHT * IMAGE_WIDTH * IMAGE_CHANNEL
-BATCH_SHAPE = [None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL]
+SHAPE_IM = [None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL]
+SHAPE_RW = [None, 1]
+SHAPE_AC = [None, 18]
 
-def weights_bias(shape, name):
-  weight = tf.get_variable(name+"_W", shape)
-  bias = tf.get_variable(name+"_B", [shape[len(shape)-1]])
-  return weight, bias
-def conv2d(x, W, stride):
-  strides = [1, stride, stride, 1]
-  return tf.nn.conv2d(x, W, strides=strides, padding="SAME")
-def max_pool_2x2(x):
-  strides = [1, 2, 2, 1]
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=strides, padding="SAME")
+LOG = "/output/Tensorboard/"
+#LOG = "Tensorboard"
+BATCH_SIZE = 10
 
 class network():
   def __init__(self):
-    self.input_St_ph = tf.placeholder("float", BATCH_SHAPE, name="input_St")
-    self.input_St1_ph = tf.placeholder("float", BATCH_SHAPE, name="input_St1")
+    self.input_st_ph  = tf.placeholder("float", SHAPE_IM, name="input_st")
+    self.input_at_ph  = tf.placeholder("float", SHAPE_AC, name="input_at")
+    self.input_rt1_ph = tf.placeholder("float", SHAPE_RW, name="input_rt1")
+    self.input_st1_ph = tf.placeholder("float", SHAPE_IM, name="input_st1")
+    #self.st   = tf.image.rgb_to_grayscale(self.input_st_ph)
+    #self.st_1 = tf.image.rgb_to_grayscale(self.input_st1_ph)
 
-    #==== Encoder ====#
-    with tf.variable_scope('Encoder_ConvNet'):
-      E_conv1_W, E_conv1_B = weights_bias([5, 5, 3, 32], "E_conv1")
-      h_conv1 = self.feed_conv(self.input_St_ph, E_conv1_W, E_conv1_B, 1)
-      E_conv2_W, E_conv2_B = weights_bias([5, 5, 32, 64], "E_conv2")
-      h_conv2 = self.feed_conv(h_conv1, E_conv2_W, E_conv2_B, 1)
-      E_conv3_W, E_conv3_B = weights_bias([5, 5, 64, 128], "E_conv3")
-      h_conv3 = self.feed_conv(h_conv2, E_conv3_W, E_conv3_B, 1)
-      h_conv3_flat = tf.contrib.layers.flatten(h_conv3)
-      E_fc1_W, E_fc1_B = weights_bias([20*20*128, 1024], "E_fc1")
-      h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, E_fc1_W) + E_fc1_B)
-      E_fc2_W, E_fc2_B = weights_bias([1024, 512], "E_fc2")
-      self.z = tf.nn.relu(tf.matmul(h_fc1, E_fc2_W) + E_fc2_B)
-    #==== Decoder ====#
-    with tf.variable_scope('Decoder_ConvNet'):
-      D_fc1_W, D_fc2_B = weights_bias([512, IMAGE_PIXELS*4], "D_fc1")
-      D_fc1 = tf.matmul(self.z, D_fc1_W) + D_fc2_B
-      D_fc1 = tf.reshape(D_fc1, [-1, IMAGE_WIDTH*2, IMAGE_HEIGHT*2, IMAGE_CHANNEL])
-      D_fc1 = tf.contrib.layers.batch_norm(D_fc1, epsilon=1e-5, scope='bn')
-      D_fc1 = tf.nn.relu(D_fc1)
+    StEndec = state_encoder_decoder("state", BATCH_SIZE)
+    latent = StEndec.encode(self.input_st_ph)
+    self.reconstruction = StEndec.decode(latent)
+    self.reconstruction_loss = StEndec.get_loss(source=self.reconstruction, 
+                                              target=self.input_st_ph)
+    self.reconstruction_train_step = StEndec.train_step(self.reconstruction_loss)
 
-      D_conv1_W, D_conv1_B = weights_bias([3, 3, 3, 64], "D_conv1")
-      h_dconv1 = conv2d(D_fc1, D_conv1_W, 1) + D_conv1_B
-      h_dpool1 = tf.contrib.layers.batch_norm(h_dconv1, epsilon=1e-5, scope='bn1')
-      h_dpool1 = tf.nn.relu(h_dpool1)
-      h_dconv1 = tf.image.resize_images(h_dpool1, [IMAGE_WIDTH * 2, IMAGE_HEIGHT * 2])
-
-      D_conv2_W, D_conv2_B = weights_bias([3, 3, 64, 32], "D_conv2")
-      h_dconv2 = conv2d(h_dconv1, D_conv2_W, 1) + D_conv2_B
-      h_dpool2 = tf.contrib.layers.batch_norm(h_dconv2, epsilon=1e-5, scope='bn2')
-      h_dpool2 = tf.nn.relu(h_dpool2)
-      h_dconv2 = tf.image.resize_images(h_dpool2, [IMAGE_WIDTH * 2, IMAGE_HEIGHT * 2])
-
-      D_conv3_W, D_conv3_B = weights_bias([1, 1, 32, 3], "D_conv3")
-      self.h_dconv3 = conv2d(h_dconv2, D_conv3_W, 2) + D_conv3_B
-
-
-  def feed_conv(self, input, weight, bias, stride):
-    temp = tf.nn.relu(conv2d(input, weight, stride) + bias)
-    return max_pool_2x2(temp)
+    data = dict()
+    data["source_image"] = self.input_st_ph
+    data["reconstructed_image"] = self.reconstruction
+    data["reconstruction_loss"] = self.reconstruction_loss
+    tensorboard_summary(data)
+    self.merged = tf.summary.merge_all()
  
-  def feed_forward(self, St_):
-    X_ = np.reshape(St_, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL])
-    readout_t = self.h_dconv3.eval(feed_dict={self.input_St_ph : X_})[0]
+  def feed_forward(self, st_):
+    X_ = np.reshape(st_, [-1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL])
+    readout_t = self.reconstruction.eval(feed_dict={self.input_st_ph : X_})
     return readout_t
+
+  def get_batch(self, D):
+    minibatch = random.sample(D, BATCH_SIZE)
+    shape_im = [BATCH_SIZE, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL]
+    shape_ac = [BATCH_SIZE, 18]
+    shape_rw = [BATCH_SIZE, 1]
+    st_batch  = np.reshape([d[0] for d in minibatch], shape_im)
+    st1_batch = np.reshape([d[3] for d in minibatch], shape_im)
+    at_batch  = np.reshape([d[1] for d in minibatch], shape_ac)
+    rt1_batch = np.reshape([d[2] for d in minibatch], shape_rw)
+    return st_batch, at_batch, rt1_batch, st1_batch
+
+  def train(self, sess, D, writer, step):
+    st_batch, at_batch, rt1_batch, st1_batch = self.get_batch(D)
+
+    ops = []
+    feed_dict = {self.input_st_ph: st_batch}
+    ops.append(self.reconstruction_train_step)
+    ops.append(self.reconstruction_loss)
+    _, loss = sess.run(ops, feed_dict=feed_dict)
+
+    if step % 10 == 0:
+      ops = []
+      feed_dict = {self.input_st_ph: st_batch}
+      ops.append(self.reconstruction_loss)
+      ops.append(self.merged)
+      loss, summary = sess.run(ops, feed_dict=feed_dict)
+      writer.add_summary(summary, step)
+      print(step, loss)
 
 class game():
   def __init__(self):
+    self.D = deque()
     self.sess = sess = tf.InteractiveSession()
     self.env = gym.make('MontezumaRevenge-v0')
     self.St = self.env.reset()
     self.net = network()
     self.sess.run(tf.global_variables_initializer())
+    self.writer = tf.summary.FileWriter(LOG, sess.graph)
 
-  def take_action(self, St, At):
-    readout = self.net.feed_forward(St)
-    print(readout.shape)
+  def take_action(self, St):
+    #readout = self.net.feed_forward(St)
+    #print(readout.shape)
+    actions = np.zeros([1, 18])
+    At = self.env.action_space.sample()
+    actions[0][At-1] = 1
     St1, Rt1, done, info = self.env.step(At)
     St1 = ut.preprocess(St1)
-    Tr = [St, At, Rt1, St1, done]
+    Rt1 = 1
+    if done:
+      Rt1 = -1
+      self.env.reset()
+    Tr = [St, actions, Rt1, St1, done]
     return Tr
 
   def run(self):
     St = self.St
     St = ut.preprocess(St)
-    for _ in range(1):
-      self.env.render()
-      At = self.env.action_space.sample()
-      Tr = self.take_action(St, At)
+    for step in range(1000):
+      #self.env.render()
+      Tr = self.take_action(St)
+      St1 = Tr[3]
+      self.D.append(Tr)
+      if len(self.D) > 100:
+        self.D.popleft()
+        self.net.train(self.sess, self.D, self.writer, step)
+      St = St1
 
 def main():
   gm = game()
---
+++ network.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+
+IMAGE_HEIGHT = 160
+IMAGE_WIDTH = 160
+IMAGE_CHANNEL = 3
+IMAGE_PIXELS = IMAGE_HEIGHT * IMAGE_WIDTH * IMAGE_CHANNEL
+BATCH_SHAPE = [None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL]
+
+class state_encoder_decoder():
+  def __init__(self, name, batch_size):
+    self.name = name
+    self.learning_rate = 0.0001
+    self.latent_dim = 512
+    self.batch_size = batch_size
+    self.enc_name = "StEnc_"
+    self.dec_name = "StDec_"
+  
+  def encode(self, image):
+    n = self.enc_name
+    with tf.variable_scope('Encoder_ConvNet'):
+      input_st = image
+      conv1 = tf.layers.conv2d(input_st,32,5, activation=tf.nn.relu, name=n+"conv1")
+      conv1 = tf.layers.max_pooling2d(conv1, 2, 2, name="E_conv1_pool")
+      conv2 = tf.layers.conv2d(conv1, 64, 5, activation=tf.nn.relu, name=n+"conv2")
+      conv2 = tf.layers.max_pooling2d(conv2, 2, 2, name="E_conv2_pool")
+      conv3 = tf.layers.conv2d(conv2, 128, 5, activation=tf.nn.relu, name=n+"conv3")
+      conv3 = tf.layers.max_pooling2d(conv3, 2, 2, name="E_conv3_pool")
+      h_conv3_flat = tf.contrib.layers.flatten(conv3)
+      fc1 = tf.layers.dense(h_conv3_flat, 1024, name="E_fc1")
+      z = tf.layers.dense(fc1, self.latent_dim, name="E_fc2")
+      return z
+
+  def decode(self, latent):
+    n = self.dec_name
+    with tf.variable_scope('Decoder_ConvNet'):
+      fc1 = tf.layers.dense(latent, IMAGE_PIXELS*4, name=n+"fc")
+      D_fc1 = tf.reshape(fc1, [-1, IMAGE_WIDTH*2, IMAGE_HEIGHT*2, IMAGE_CHANNEL])
+      D_fc1 = tf.contrib.layers.batch_norm(D_fc1, epsilon=1e-5, scope='bn')
+      D_fc1 = tf.nn.relu(D_fc1)
+      dconv1 = tf.layers.conv2d(D_fc1, 64, 5, activation=tf.nn.relu, name=n+"conv1")
+      dconv1 = tf.contrib.layers.batch_norm(dconv1, epsilon=1e-5, scope='bn1')
+      dconv1 = tf.image.resize_images(dconv1, [IMAGE_WIDTH*2, IMAGE_HEIGHT*2])
+      dconv2 = tf.layers.conv2d(dconv1, 32, 3, activation=tf.nn.relu, name=n+"conv2")
+      dconv2 = tf.contrib.layers.batch_norm(dconv2, epsilon=1e-5, scope='bn2')
+      dconv2 = tf.image.resize_images(dconv2, [IMAGE_WIDTH*1, IMAGE_HEIGHT*1])
+      image  = tf.layers.conv2d(dconv2,3,1,activation=tf.nn.sigmoid, name=n+"conv3")
+      return image
+
+  def get_loss(self, source, target):
+      with tf.name_scope('Loss'):
+        #target = tf.image.rgb_to_grayscale(target)
+        batch_flatten = tf.reshape(target, [self.batch_size, -1])
+        batch_reconstruct_flatten = tf.reshape(source, [self.batch_size, -1])
+        loss1 = batch_flatten*tf.log(1e-10+batch_reconstruct_flatten)
+        loss2 = (1 - batch_flatten) * tf.log(1e-10 + 1 - batch_reconstruct_flatten)
+        loss = loss1+loss2
+        reconstruction_loss = -tf.reduce_sum(loss)
+        return tf.reduce_mean(reconstruction_loss)
+  
+  def train_step(self, loss):
+    lr = self.learning_rate
+    tvars = tf.trainable_variables()
+    st_encoder_vars = [var for var in tvars if self.enc_name in var.name]
+    st_decoder_vars = [var for var in tvars if self.dec_name in var.name]
+    st_vars = st_encoder_vars+st_decoder_vars
+    with tf.variable_scope(tf.get_variable_scope()) as scope:
+      train_step = tf.train.AdamOptimizer(lr).minimize(loss,var_list = st_vars)
+    return train_step
+
+def tensorboard_summary(data):
+  st = data["source_image"]
+  st_recon = data["reconstructed_image"]
+  with tf.name_scope("summary/images"):
+    source_image = (st+1) * 127.5
+    recon_image  = (st_recon+1) * 127.5
+    image_to_tb = tf.concat([source_image, recon_image], axis=1)
+    tf.summary.image('src', source_image, 5)
+    tf.summary.image('recon', recon_image, 5)
+  with tf.name_scope("summary/losses"):
+    tf.summary.scalar("StRecon_loss", data["reconstruction_loss"])
