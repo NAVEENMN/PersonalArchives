64164c431c7239e2c4a736247f375165449e2f43
Naveen Mysore <navimn1991@gmail.com>
2018-07-10 08:29:05 -0700

    base

---
+++ frozen_graph/graph.py
@@ -0,0 +1,36 @@
+'''
+ load pb (protoBuf) file and run an inference
+'''
+import os
+import numpy as np
+import tensorflow as tf
+
+class graph():
+    def __init__(self, dir_path, model_name):
+        self.graph = self.load_graph(dir_path, model_name)
+        self.input_ph = self.graph.get_tensor_by_name('image_tensor:0')
+        self.mid = self.graph.get_tensor_by_name('add_6:0')
+        self.output = self.graph.get_tensor_by_name('detection_classes:0')
+
+    def load_graph(self, dir_path, model_name):
+        path = os.path.join(dir_path, model_name)
+        # Load protobuf file from the disk and retrive unserialized graph
+        with tf.gfile.FastGFile(path, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+        # import graph_def into a new graph
+        with tf.Graph().as_default() as graph:
+            tf.import_graph_def(graph_def, name='')
+        return graph
+
+    def run(self, input_image):
+        print("vars")
+        feed_dict = {self.input_ph: input_image}
+        with tf.Session(graph=self.graph) as sess:
+            op = sess.graph.get_operations()
+            tensors = [m.values() for m in op]
+            for tensor in tensors:
+                print(tensor)
+            res = sess.run([self.mid, self.output],feed_dict=feed_dict)
+            print(res)
+
