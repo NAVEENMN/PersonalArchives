2ec537a524e3483e567745c763a661b888349fe5
NAVEENMN <navimn1991@gmail.com>
2018-08-11 21:17:55 -0700

    base

--- main.py
+++ main.py
@@ -1,82 +1,263 @@
 from defs import *
 from models import *
+from utils import *
 import tensorflow as tf
+from tensorflow.python.client import device_lib
 import numpy as np
 import gym
+import random
 
 class network():
-    def __init__(self, sess):
-        self.sess = sess
+	def __init__(self, sess):
+		self.sess = sess
+		self.devs = device_lib.list_local_devices()
+		data = dict()
+		self.net_details = dict()
+		with tf.name_scope("Inputs"):
+			self.input_st_ph = tf.placeholder("float", [None, 1, 4], name="input_st")
+			self.input_stimg_ph = tf.placeholder("float", ST_SHAPES, name="input_st_image")
+			self.input_at_ph = tf.placeholder("float", AT_SHAPES, name="input_at")
+			self.input_pi_ph = tf.placeholder("float", [None, 1, 4], "input_pi")
+			self.input_adv = tf.placeholder("float", ADV_SHAPES, "input_adv")
+			self.input_returns = tf.placeholder("float", ADV_SHAPES, "input_returns")
+			self.input_st_latents = tf.placeholder("float", ST_LT_SHAPES, name="input_state_latents")
 
-        with tf.name_scope("Inputs"):
-            self.input_st_ph = tf.placeholder("float", ST_SHAPES, name="input_st")
-            self.input_st_latents = tf.placeholder("float", ST_LT_SHAPES, name="input_state_latents")
+		with tf.name_scope("Phi"):
+			stendec = state_encoder_decoder("Phi", LATENT_DIM)
+			'''
+			with tf.name_scope("Phi_Encode"):
+				self.phi = stendec.encode(self.input_st_ph, False)
+				data["phi_input"] = self.input_st_ph
+			'''
+			with tf.name_scope("Phi_Decode"):
+				self.st_ = stendec.decode(self.input_st_ph, False)
+				data["phi_output"] = self.st_
+				self.st_recon = stendec.decode(self.input_st_latents, True)
+				
+			with tf.name_scope("Phi_loss"):
+				self.phi_loss = stendec.get_loss(source=self.st_,
+												target=self.input_st_ph)
+				data["phi_loss"] = self.phi_loss
+				
+			with tf.name_scope("Phi_train"):
+				self.phi_train_step = stendec.train_step(self.phi_loss)
+			self.net_details["phi"] = stendec.get_vars()
+		
+		with tf.name_scope("Theta"):
+			atencdec = action_encode_decoder("Theta")
+			with tf.name_scope("Theta_Encode"):
+				self.theta = atencdec.encode(self.input_at_ph, False)
+			with tf.name_scope("Theta_Decode"):
+				self.at_ = atencdec.decode(self.theta, False)
+			with tf.name_scope("Theta_loss"):
+				self.theta_loss = atencdec.get_loss(source=self.at_,
+												    target=self.input_at_ph)
+				data["theta_loss"] = self.theta_loss
+			with tf.name_scope("Theta_train"):
+				self.theta_train_step = atencdec.train_step(self.theta_loss)
+			self.net_details["theta"] = atencdec.get_vars()
+		
+		with tf.name_scope("critic"):
+			vs = critic("critic_")
+			self.st_value = vs.get_value(self.input_st_ph, False)
+			self.critic_loss = vs.get_loss(value=self.st_value,
+										   target_value=self.input_returns)
+			data["critic_loss"] = self.critic_loss
+			self.critic_train_step = vs.train_step(self.critic_loss)
+			self.net_details["critic"] = vs.get_vars()
+			
+		with tf.name_scope("pi"):
+			with tf.device('/device:GPU:1'):
+				pi = policy("pi_")
+				self.pi_out = pi.get_action(self.input_pi_ph, False)
+				self.pi_grads, self.pi_scaled_grads = pi.get_scaled_grads(self.pi_out, self.input_adv, 1)
+				self.pi_train_step = pi.train_step(self.pi_scaled_grads)
+				self.net_details["pi"] = pi.get_vars()
+		
+		tensorboard_summary(data)
+		with tf.name_scope("Tensorboard"):
+			self.merged = tf.summary.merge_all()
 
-        with tf.name_scope("Phi"):
-            stendec = state_encoder_decoder("Phi", LATENT_DIM)
-            with tf.name_scope("Phi_Encode"):
-                self.phi = stendec.encode(self.input_st_ph, False)
-            with tf.name_scope("Phi_Decode"):
-                self.st_ = stendec.decode(self.phi, False)
-                self.st_recon = stendec.decode(self.input_st_latents, True)
-            with tf.name_scope("Phi_loss"):
-                self.phi_st_loss = stendec.get_loss(source=self.st_,
-                                                    target=self.input_st_ph)
-            with tf.name_scope("Phi_train"):
-                self.phi_st_train_step = stendec.train_step(self.phi_st_loss)
-
-    def get_state_latent(self, input_st):
-        input_st = np.reshape(input_st, ST_SHAPE)
-        latent = self.sess.run(self.phi, feed_dict={self.input_st_ph: input_st})
-        return latent
-
-    def get_state_reconstruction(self, state_latent):
-        state_latent = np.reshape(state_latent, (-1, LATENT_DIM))
-        recon = self.sess.run(self.st_recon, feed_dict={self.input_st_latents: state_latent})
-        return recon
-
+	'''
+	def get_state_latent(self, input_st):
+		input_st = np.reshape(input_st, ST_SHAPE)
+		latent = self.sess.run(self.phi, feed_dict={self.input_st_ph: input_st})
+		return latent
+	'''
+	
+	def get_state_reconstruction(self, state_latent):
+		state_latent = np.reshape(state_latent, (-1, LATENT_DIM))
+		recon = self.sess.run(self.st_recon, feed_dict={self.input_st_latents: state_latent})
+		return recon
+	
+	def get_action(self, state_latent):
+		state_latent = np.reshape(state_latent, (-1, LATENT_DIM))
+		action = self.sess.run(self.pi_out, feed_dict={self.input_pi_ph: state_latent})
+		return action
+	
+	def get_state_values(self, state_latent):
+		state_latent = np.reshape(state_latent, (-1, LATENT_DIM))
+		value = self.sess.run(self.st_value, feed_dict={self.input_st_latents: state_latent})
+		return value
+	
+	def train_step_sr(self, memory):
+		s_batch, slat_batch, a_batch, r_batch, s2_batch, slat1_batch, done_batch = memory.sample_batch(10)
+		feed_dict = {self.input_st_ph: slat_batch,
+					 self.input_stimg_ph: s_batch,
+					 self.input_at_ph: a_batch,
+					 self.input_returns: r_batch}
+		
+		ops = []
+		ops.append(self.phi_train_step)
+		ops.append(self.theta_train_step)
+		ops.append(self.critic_train_step)
+		self.sess.run(ops, feed_dict=feed_dict)
+		
+		ops = []
+		ops.append(self.phi_loss)
+		ops.append(self.theta_loss)
+		ops.append(self.critic_loss)
+		ops.append(self.merged)
+		
+		phi_loss, theta_loss, critic_loss, summary = self.sess.run(ops, feed_dict=feed_dict)
+		
+		return phi_loss, theta_loss, critic_loss, summary
+	
+	def train_step_pi(self, states, advantages):
+		states = np.reshape(states, (-1, LATENT_DIM))
+		advantages = np.reshape(advantages, [-1, 1])
+		feed_dict = {self.input_pi_ph: states, self.input_adv: advantages}
+		self.sess.run(self.pi_train_step, feed_dict=feed_dict)
+	
 class game():
-    def __init__(self, sess):
-        self.sess = sess
-        # Game environment
-        self.env_name = 'Acrobot-v1'
-        self.env = gym.make(self.env_name)
-        self.action_space = self.env.action_space
-        self.observation_space = self.env.observation_space
-        self.env.reset()
-        self.print_game_env()
-        st, _, _, _ = self.env.step(self.env.action_space.sample())
-        self.net = network(sess)
-        self.sess.run(tf.global_variables_initializer())
+	def __init__(self, sess):
+		self.sess = sess
+		# Game environment
+		self.env_name = 'CartPole-v0'
+		self.env = gym.make(self.env_name)
+		self.action_space = self.env.action_space
+		self.observation_space = self.env.observation_space
+		self.env.reset()
+		self.print_game_env()
+		st, _, _, _ = self.env.step(self.env.action_space.sample())
+		self.net = network(sess)
+		self.memory = ReplayBuffer(1000000, 1234)
+		self.pi_buffer = ReplayBuffer(10000)
+		self.saver = tf.train.Saver()
+		self.sess.run(tf.global_variables_initializer())
+		self.print_network()
+		self.writer = tf.summary.FileWriter(LOG, sess.graph)
+		checkpoint = tf.train.get_checkpoint_state(MODEL_PATH)
+		if checkpoint and checkpoint.model_checkpoint_path:
+			self.saver.restore(sess, checkpoint.model_checkpoint_path)
+			print("Successfully loaded:", checkpoint.model_checkpoint_path)
+		else:
+			print("Could not find old network weights")
 
-    def print_game_env(self):
-        print("-----")
-        print("environment: {}".format(self.env_name))
-        print("action space : {}".format(self.action_space))
-        screen = self.env.render(mode='rgb_array')
-        print("state space : {}".format(self.observation_space))
-        print("observation space : {}".format(screen.shape))
-        print("reward range : {}".format(self.env.reward_range))
-        print("------\n")
-
-    def run(self):
-        at = self.env.action_space.sample()
-        st, rt, done, _ = self.env.step(at)
-        screen = self.env.render(mode='rgb_array')
-        print(screen.shape)
-        print("res", [st, at, rt])
-        #im = np.random.rand(1, 96, 96, 3)
-        #z = self.net.get_state_latent(im)
-        #rec = self.net.get_state_reconstruction(z)
-        #print(z)
-        #print(rec)
-
+	def print_game_env(self):
+		print("-----")
+		print("environment: {}".format(self.env_name))
+		print("action space : {}".format(self.action_space))
+		screen = self.env.render(mode='rgb_array')
+		print("state space : {}".format(self.observation_space))
+		print("observation space : {}".format(screen.shape))
+		print("reward range : {}".format(self.env.reward_range))
+		print("------\n")
+		
+	def print_network(self):
+		devs= [dev.name for dev in self.net.devs]
+		tvars = self.net.net_details
+		print("-----")
+		print(devs)
+		print(" ")
+		for var in tvars:
+			print(var)
+			tensors = tvars[var]
+			#tensors = self.sess.run(tensors)
+			for v in tensors:
+				print(v)
+			print(" ")
+		print("------\n")
+	
+	def compute_returns(self, tr):
+		returns = [-1.0]
+		for sa_pair in tr[::-1]:
+			value = 0.1 + 0.9 * returns[-1]
+			returns.append(value)
+		returns = returns[::-1]
+		returns = returns[1:]
+		states = [sa[1] for sa in tr]
+		for x in range(0, len(states)):
+			sas = tr[x]
+			sas[3] = returns[x]
+			self.memory.add(sas)
+		return states, returns
+	
+	def train_sr(self, step):
+		phi_loss, theta_loss, critic_loss, summary = self.net.train_step_sr(self.memory)
+		print("step SR {} : phi_loss {}, theta_loss {} critic_loss {} ".format(step, phi_loss, theta_loss, critic_loss))
+		self.writer.add_summary(summary, step)
+		
+	def train_policy(self, step, state, reward):
+		self.net.train_step_pi(state, reward)
+		print("step PI {}".format(step))
+	
+	def run_an_episodes(self, epoch, roll_outs, eps):
+		STATES, ADVS = [], []
+		for roll in range(0, roll_outs):
+			done = False
+			self.env.reset()
+			action = self.env.action_space.sample()
+			vec_st, _, _, _ = self.env.step(action)
+			img_st = self.env.render(mode='rgb_array')
+			img_st, vec_st = pre_process(img_st, vec_st, self.net)
+			tr = list()
+			while not done:
+				at = self.net.get_action(vec_st)
+				if eps and (random.random() > 0.6):
+					temp = np.zeros((1, 2))
+					action = self.env.action_space.sample()
+					temp[0][action] = 1.0
+					at = temp
+				action = np.argmax(at[0])
+				temp = np.zeros((1, 2))
+				temp[0][action] = 1.0
+				at = temp
+				vec_st1, rt1, done, info = self.env.step(action)
+				img_st1 = self.env.render(mode='rgb_array')
+				img_st1, vec_st1 = pre_process(img_st1, vec_st1, self.net)
+				sas= [img_st, vec_st, at, rt1, img_st1, vec_st1, done]
+				tr.append(sas)
+				vec_st, img_st = vec_st1, img_st1
+			# memory is updated after returns are computed
+			states, returns = self.compute_returns(tr)
+			estimates = np.reshape(self.net.get_state_values(states), [-1])
+			advantages = returns - estimates
+			STATES.extend(states)
+			ADVS.extend(advantages)
+		print("epoch {} : total return {}".format(epoch, len(STATES)))
+		return STATES, ADVS
 
 def main():
-    sess = tf.InteractiveSession()
-    gm = game(sess)
-    for _ in range(10):
-        gm.run()
+	#sess = tf.InteractiveSession()
+	sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+	gm = game(sess)
+	sr_step, pi_step = 0, 0
+	for step in range(10000):
+		prob_train = random.random()
+		if prob_train > 0.4:
+			# eps greedy policy
+			STATES, ADVS = gm.run_an_episodes(step, 1, True)
+			gm.train_sr(sr_step)
+			sr_step += 1
+		else:
+			# on policy
+			STATES, ADVS = gm.run_an_episodes(step, 1, False)
+			gm.train_policy(pi_step, STATES, ADVS)
+			pi_step += 1
+		if step % 1000000 == 0:
+			save_path = gm.saver.save(sess, MODEL_PATH + "pretrained.ckpt", global_step=step)
+			print("saved to %s" % save_path)
 
 if __name__ == "__main__":
-    main()
+	main()
--- models.py
+++ models.py
@@ -1,59 +1,188 @@
 from defs import *
+from models import *
+from utils import *
 import tensorflow as tf
 import numpy as np
 
+'''
+sate en dec not good
+action_endode loss not minimizing
+policy needs bias advangates
+'''
+
 # ------ State encoder decoder -----#
 class state_encoder_decoder():
-    def __init__(self, name, latent_dim_size):
-        self.name = name
-        self.en_name = name+"_encode"
-        self.dec_name = name+"_decode"
-        self.latent_dim = latent_dim_size
-        self.opt = tf.train.AdamOptimizer(0.01)
+	def __init__(self, name, latent_dim_size):
+		self.name = name
+		self.en_name = name+"_encode_"
+		self.dec_name = name+"_decode_"
+		self.latent_dim = latent_dim_size
+		self.opt = tf.train.AdamOptimizer(0.0001)
+	
+	'''
+	def encode(self, state, reuse):
+		with tf.variable_scope(self.en_name, reuse=reuse):
+			conv1 = tf.layers.conv2d(state, 32, 5, activation=tf.nn.relu, name=self.en_name+"conv1")
+			conv1 = tf.layers.max_pooling2d(conv1, 2, 2, name=self.en_name+"conv1_pool")
+			conv2 = tf.layers.conv2d(conv1, 64, 5, activation=tf.nn.relu, name=self.en_name+"conv2")
+			conv2 = tf.layers.max_pooling2d(conv2, 2, 2, name=self.en_name+"conv2_pool")
+			conv3 = tf.layers.conv2d(conv2, 128, 3, activation=tf.nn.relu, name=self.en_name+"conv3")
+			conv3 = tf.layers.max_pooling2d(conv3, 2, 2, name=self.en_name+"conv3_pool")
+			h_conv3_flat = tf.contrib.layers.flatten(conv3)
+			fc1 = tf.layers.dense(h_conv3_flat, 1024, activation=tf.nn.tanh, name=self.en_name+"fc1")
+			z = tf.layers.dense(fc1, self.latent_dim, activation=tf.nn.tanh, name=self.en_name+"fc2")
+			return z
+	'''
+	
+	def decode(self, state_latent, reuse):
+		with tf.variable_scope(self.dec_name, reuse=reuse):
+			fc1 = tf.layers.dense(state_latent, IMAGE_PIXELS * 4, name=self.dec_name+"fc")
+			D_fc1 = tf.reshape(fc1, [-1, IMAGE_HEIGHT * 2, IMAGE_WIDTH * 2, IMAGE_CHANNEL])
+			D_fc1 = tf.contrib.layers.batch_norm(D_fc1, epsilon=1e-5)
+			D_fc1 = tf.nn.tanh(D_fc1)
+			dconv1 = tf.layers.conv2d(D_fc1, 64, 5, activation=tf.nn.tanh, name=self.dec_name+"conv1")
+			dconv1 = tf.contrib.layers.batch_norm(dconv1, epsilon=1e-5)
+			dconv1 = tf.image.resize_images(dconv1, [IMAGE_HEIGHT * 2, IMAGE_WIDTH * 2])
+			dconv2 = tf.layers.conv2d(dconv1, 32, 3, activation=tf.nn.tanh, name=self.dec_name+"conv2")
+			dconv2 = tf.contrib.layers.batch_norm(dconv2, epsilon=1e-5)
+			dconv2 = tf.image.resize_images(dconv2, [IMAGE_HEIGHT * 1, IMAGE_WIDTH * 1])
+			image = tf.layers.conv2d(dconv2, 3, 1, activation=tf.nn.sigmoid, name=self.dec_name+"conv3")
+			return image
 
-    def encode(self, state, reuse):
-        with tf.variable_scope(self.en_name, reuse=reuse):
-            conv1 = tf.layers.conv2d(state, 32, 10, activation=tf.nn.relu, name=self.en_name+"_conv1")
-            conv1 = tf.layers.max_pooling2d(conv1, 2, 2, name=self.en_name+"conv1_pool")
-            conv2 = tf.layers.conv2d(conv1, 64, 8, activation=tf.nn.relu, name=self.en_name+"conv2")
-            conv2 = tf.layers.max_pooling2d(conv2, 2, 2, name=self.en_name+"conv2_pool")
-            conv3 = tf.layers.conv2d(conv2, 128, 5, activation=tf.nn.relu, name=self.en_name+"conv3")
-            conv3 = tf.layers.max_pooling2d(conv3, 2, 2, name=self.en_name+"conv3_pool")
-            conv3 = tf.layers.conv2d(conv3, 128, 3, activation=tf.nn.relu, name=self.en_name+"conv4")
-            conv4 = tf.layers.max_pooling2d(conv3, 2, 2, name=self.en_name+"conv4_pool")
-            h_conv4_flat = tf.contrib.layers.flatten(conv4)
-            fc1 = tf.layers.dense(h_conv4_flat, 1024, activation=tf.nn.sigmoid, name=self.en_name+"E_fc1")
-            z = tf.layers.dense(fc1, self.latent_dim, activation=tf.nn.sigmoid, name=self.en_name+"E_fc2")
-            return z
+	def get_loss(self, source, target):
+		batch_flatten = tf.reshape(target, [BATCH_SIZE, -1])
+		batch_reconstruct_flatten = tf.reshape(source, [BATCH_SIZE, -1])
+		#loss = tf.losses.mean_squared_error(labels=batch_flatten, predictions=batch_reconstruct_flatten)
+		#loss = tf.reduce_mean(loss)
+		#return loss
+		
+		loss1 = batch_flatten * tf.log(1e-10 + batch_reconstruct_flatten)
+		loss2 = (1 - batch_flatten) * tf.log(1e-10 + 1 - batch_reconstruct_flatten)
+		loss = loss1 + loss2
+		reconstruction_loss = -tf.reduce_sum(loss)
+		loss = tf.reduce_mean(reconstruction_loss)
+		return loss
+		
+	def train_step(self, loss):
+		tvars = tf.trainable_variables()
+		phi_vars = [var for var in tvars if self.name in var.name]
+		with tf.variable_scope(tf.get_variable_scope()) as scope:
+			train_ = self.opt.minimize(loss, var_list=phi_vars)
+		return train_
+	
+	def get_vars(self):
+		tvars = tf.trainable_variables()
+		vars = [var for var in tvars if self.name in var.name]
+		return vars
 
-    def decode(self, state_latent, reuse):
-        with tf.variable_scope(self.dec_name, reuse=reuse):
-            fc1 = tf.layers.dense(state_latent, IMAGE_PIXELS * 4, name=self.dec_name+"fc")
-            D_fc1 = tf.reshape(fc1, [-1, IMAGE_WIDTH * 2, IMAGE_HEIGHT * 2, IMAGE_CHANNEL])
-            D_fc1 = tf.contrib.layers.batch_norm(D_fc1, epsilon=1e-5)
-            D_fc1 = tf.nn.relu(D_fc1)
-            dconv1 = tf.layers.conv2d(D_fc1, 64, 10, activation=tf.nn.relu, name=self.dec_name+"conv1")
-            dconv1 = tf.contrib.layers.batch_norm(dconv1, epsilon=1e-5)
-            dconv1 = tf.image.resize_images(dconv1, [IMAGE_WIDTH * 2, IMAGE_HEIGHT * 2])
-            dconv2 = tf.layers.conv2d(dconv1, 32, 5, activation=tf.nn.relu, name=self.dec_name+"conv2")
-            dconv2 = tf.contrib.layers.batch_norm(dconv2, epsilon=1e-5)
-            dconv2 = tf.image.resize_images(dconv2, [IMAGE_WIDTH * 1, IMAGE_HEIGHT * 1])
-            image = tf.layers.conv2d(dconv2, 3, 1, activation=tf.nn.sigmoid, name=self.dec_name+"conv3")
-            return image
+# ------ Action encoder decoder -----#
+class action_encode_decoder():
+	def __init__(self, name):
+		self.name = name
+		self.opt = tf.train.AdamOptimizer(0.001)
+		self.enc_name = self.name + "_Enc_"
+		self.dec_name = self.name + "_Dec_"
 
-    def get_loss(self, source, target):
-        batch_flatten = tf.reshape(target, [BATCH_SIZE, -1])
-        batch_reconstruct_flatten = tf.reshape(source, [BATCH_SIZE, -1])
-        loss1 = batch_flatten * tf.log(1e-10 + batch_reconstruct_flatten)
-        loss2 = (1 - batch_flatten) * tf.log(1e-10 + 1 - batch_reconstruct_flatten)
-        loss = loss1 + loss2
-        reconstruction_loss = -tf.reduce_sum(loss)
-        loss = tf.reduce_mean(reconstruction_loss)
-        return loss
+	def encode(self, action, reuse=False):
+		n = self.enc_name
+		with tf.variable_scope(n, reuse=reuse):
+			fc1 = tf.layers.dense(action, LATENT_DIM * 4, activation=tf.nn.tanh, name=n + "fc1")
+			fc2 = tf.layers.dense(fc1, LATENT_DIM * 2, activation=tf.nn.tanh, name=n + "fc2")
+			action_latent = tf.layers.dense(fc2, LATENT_DIM, activation=tf.nn.tanh, name=n + "fc3")
+		return action_latent
 
-    def train_step(self, loss):
-        tvars = tf.trainable_variables()
-        phi_vars = [var for var in tvars if self.name in var.name]
-        with tf.variable_scope(tf.get_variable_scope()) as scope:
-            train_step = self.opt.minimize(loss, var_list=phi_vars)
-        return train_step
+	def decode(self, action_latent, reuse=False):
+		n = self.dec_name
+		with tf.variable_scope(n, reuse=reuse):
+			fc1 = tf.layers.dense(action_latent, LATENT_DIM / 2, activation=tf.nn.tanh, name=n + "fc1")
+			action = tf.layers.dense(fc1, ACTION_SPACE, activation=tf.nn.softmax, name=n + "fc3")
+		return action
+
+	def get_loss(self, source, target):
+		#loss = tf.losses.mean_squared_error(labels=target, predictions=source)
+		loss = tf.losses.softmax_cross_entropy(onehot_labels=target, logits=source)
+		#loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=source, labels=target)
+		return tf.reduce_mean(loss)
+
+	def train_step(self, loss):
+		tvars = tf.trainable_variables()
+		at_vars = [var for var in tvars if self.name in var.name]
+		with tf.variable_scope(tf.get_variable_scope()) as scope:
+			train_ = self.opt.minimize(loss, var_list=at_vars)
+		return train_
+	
+	def get_vars(self):
+		tvars = tf.trainable_variables()
+		at_vars = [var for var in tvars if self.name in var.name]
+		return at_vars
+
+# critic later replace by SR
+class critic():
+	def __init__(self, name):
+		self.name = name
+		self.opt = tf.train.AdamOptimizer(learning_rate=0.001)
+		
+	def get_value(self, state_latent, reuse):
+		n = self.name
+		with tf.variable_scope(n, reuse=reuse):
+			fc1 = tf.layers.dense(state_latent, 20, activation=tf.nn.relu, name=n+"fc1")
+			fc2 = tf.layers.dense(fc1, 15, activation=tf.nn.tanh, name=n+"fc2")
+			fc3 = tf.layers.dense(fc2, 5, activation=tf.nn.tanh, name=n+"fc3")
+			value = tf.layers.dense(fc3, 1, activation=None, name=n+"fc4")
+		return value
+	
+	def get_loss(self, value, target_value):
+		loss = tf.losses.mean_squared_error(predictions=value, labels=target_value)
+		return loss
+	
+	def train_step(self, loss):
+		tvars = [var for var in  tf.trainable_variables() if self.name in var.name]
+		train_ = self.opt.minimize(loss, var_list=tvars)
+		return train_
+	
+	def get_vars(self):
+		vars = [var for var in tf.trainable_variables() if self.name in var.name]
+		return vars
+	
+class policy():
+	def __init__(self, name):
+		self.name = name
+		self.rolls_per_batch = 1
+		self.opt = tf.train.AdamOptimizer(learning_rate=0.001, epsilon=1e-4)
+		
+	def get_action(self, state, reuse):
+		with tf.variable_scope(self.name, reuse=reuse):
+			fc1 = tf.layers.dense(state, 30, activation=tf.nn.tanh, name=self.name + "fc1")
+			fc2 = tf.layers.dense(fc1, 20, activation=tf.nn.tanh, name=self.name + "fc2")
+			fc3 = tf.layers.dense(fc2, 10, activation=tf.nn.tanh, name=self.name + "fc3")
+			out = tf.layers.dense(fc3, ACTION_SPACE, activation=tf.nn.softmax, name=self.name + "out")
+		return out
+
+	def get_scaled_grads(self, pi_out, advantages, rolls):
+		pi_vars = [var for var in tf.trainable_variables() if self.name in var.name]
+		grads = tf.gradients(tf.log(pi_out), pi_vars)
+		scaled_grads = tf.gradients((tf.log(pi_out) * advantages) / rolls, pi_vars)
+		return grads, scaled_grads
+	
+	def train_step(self, grads):
+		pi_vars = [var for var in tf.trainable_variables() if self.name in var.name]
+		train_step = self.opt.apply_gradients(zip(grads, pi_vars))
+		return train_step
+	
+	def get_vars(self):
+		vars = [var for var in tf.trainable_variables() if self.name in var.name]
+		return vars
+	
+def tensorboard_summary(data):
+	source_image = data["phi_input"]
+	recon_image = data["phi_output"]
+	with tf.name_scope("summary/images"):
+		image_to_tb = tf.concat([source_image, recon_image], axis=1)
+		#image_to_tb = tf.concat([image_to_tb, sr_feature_recon], axis=1)
+		image_to_tb = (image_to_tb) * 255
+		tf.summary.image('src', image_to_tb, 4)
+		# tf.summary.image('recon', recon_image, 5)
+	with tf.name_scope("summary/losses"):
+		tf.summary.scalar("phi_loss", data["phi_loss"])
+		tf.summary.scalar("theta_loss", data["theta_loss"])
+		tf.summary.scalar("critic_loss", data["critic_loss"])
