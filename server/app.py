69a561f629d19fee7d9e2eca25c421d9edb3021e
Naveen Mysore <navimn1991@gmail.com>
2019-06-22 16:38:18 -0700

    <<

--- server/app.py
+++ server/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
- 
-app = Flask(__name__)
- 
-@app.route('/')
-def index():
-    return "Hello World!"
+import web
 
-if __name__ == '__main__':
+urls = (
+    '/(.*)', 'hello'
+)
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self, name):
+        if not name:
+            name = 'World'
+        return 'Hello, ' + name + '!'
+
+if __name__ == "__main__":
     app.run()
