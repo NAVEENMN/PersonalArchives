02857575cbd9d9e2a769e8a674d63127bc634ce3
EC2 Default User <ec2-user@ip-172-31-29-234.us-west-2.compute.internal>
2019-07-16 16:19:31 +0000

    <<

--- using_temp/main.tf
+++ using_temp/main.tf
@@ -1,13 +1,60 @@
-# to run terraform apply -var-file variables.tfvars
-# if you variables files in name terraform.tfvars then it will be automatically picked up
-
-provider "aws"{
-    region = "eu-west-1"
+provider "aws"  {
+    region = "us-east-1"
 }
 
-resource "aws_instance" "vm" {
-    count = 2
-    availability_zone="${var.zones[count.index]}"
-    ami = "ami-01e6a0b85de033c99"
-    instance_type = "t3.micro"
+provider "aws" {
+    alias="bubba-west"
+    region = "us-west-2"
+}
+
+
+locals {
+    def_front_name="${join("-",list(var.env-name,"frontend"))}"
+    def_back_name="${join("-",list(var.env-name,"backend"))}"
+}
+
+resource "aws_instance" "west_fe"{
+    tags {
+        Name="${local.def_front_name}"
+    }
+    count=2
+    depends_on = ["aws_instance.west_be"]
+    availability_zone ="${data.aws_availability_zones.us-west-zones.names[count.index]}"
+    provider="aws.bubba-west"
+    ami="${data.aws_ami.latest-ubuntu-west.id}" #"${var.amis[data.aws_region.west.name]}"
+    instance_type="t2.micro"
+}
+
+resource "aws_instance" "west_be"{
+    
+     tags {
+        Name="${local.def_back_name}"
+    }
+    count=2
+    availability_zone ="${data.aws_availability_zones.us-west-zones.names[count.index]}"
+    provider="aws.bubba-west"
+    ami="${data.aws_ami.latest-ubuntu-west.id}"
+    instance_type="t2.micro"
+}
+
+resource "aws_instance" "east_fe"{
+    tags {
+        Name="${local.def_front_name}"
+    }
+    count="${var.multi-region-deployment ? 1:0}"
+    depends_on = ["aws_instance.east_be"]
+    availability_zone ="${data.aws_availability_zones.us-east-zones.names[count.index]}"
+    ami="${data.aws_ami.latest-ubuntu-east.id}"
+    instance_type="t2.micro"
+}
+
+resource "aws_instance" "east_be"{
+    
+     tags {
+        Name="${local.def_back_name}"
+    }
+    count="${var.multi-region-deployment ? 1:0}"
+    availability_zone ="${data.aws_availability_zones.us-east-zones.names[count.index]}"
+    ami="${data.aws_ami.latest-ubuntu-east.id}"
+    instance_type="t2.micro"
 }
--- using_temp/output.tf
+++ using_temp/output.tf
@@ -1,7 +1,17 @@
-output "public_ip_addresses" {
-    value="${aws_instance.vm.*.public_ip}"
+output "west_front_ips"{
+    value="${aws_instance.west_fe.*.public_ip}"
+}
+output "west_be_ips"{
+    value="${aws_instance.west_be.*.public_ip}"
 }
 
-output "bubba_data" {
-    value="${aws_instance.vm.*.private_ip}"
+output "east_front_ips"{
+    value="${aws_instance.east_fe.*.public_ip}"
+}
+output "east_be_ips"{
+    value="${aws_instance.east_be.*.public_ip}"
+}
+
+output "data_az_west"{
+    value="${data.aws_availability_zones.us-west-zones.*.names}"
 }
