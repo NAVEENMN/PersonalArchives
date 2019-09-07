b168c5743c96f5a5d8d9964705237362fe5cd2d3
Naveen Mysore <navimn1991@gmail.com>
2019-09-07 14:23:45 -0700

    dbser

--- infrastructure/aws/terraform/main.tf
+++ infrastructure/aws/terraform/main.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "webserver"{
   vpc_security_group_ids = ["${aws_security_group.My_VPC_Security_Group.id}"]
 }
 
+/*
 # resources to deploy in this region
 resource "aws_instance" "appserver"{
   ami = "${lookup(var.ami,var.aws_region)}"
@@ -20,31 +21,42 @@ resource "aws_instance" "appserver"{
   subnet_id = "${aws_subnet.My_VPC_Subnet.id}"
   vpc_security_group_ids = ["${aws_security_group.My_VPC_Security_Group.id}"]
 }
+*/
+
+# resources to deploy in this region
+resource "aws_instance" "dbserver"{
+  ami = "${lookup(var.ami,var.aws_region)}"
+  instance_type = "t2.medium"
+  key_name = "nmysore"
+  subnet_id = "${aws_subnet.My_VPC_Subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.My_VPC_Security_Group.id}"]
+}
 
 output "webserver_ip_addr" {
-  value       = aws_instance.webserver.public_ip
-  description = "The public IP address of the main server instance."
+  value = { "ip": aws_instance.webserver.public_ip,
+            "dns": aws_instance.webserver.public_dns,
+            "login": "ssh -L localhost:8888:localhost:8888 -i <pem file> ubuntu@${aws_instance.webserver.public_dns}"}
+  description = "The public IP address of the web server instance."
 }
 
-output "webserver_dns" {
-  value = aws_instance.webserver.public_dns
-  description = "The public dns of the main server instance."
+output "dbserver_ip_addr" {
+  value = { "ip": aws_instance.dbserver.public_ip,
+            "dns": aws_instance.dbserver.public_dns,
+            "login": "ssh -L localhost:8888:localhost:8888 -i <pem file> ubuntu@${aws_instance.dbserver.public_dns}"}
+  description = "The public IP address of the db server instance."
 }
 
+resource "local_file" "config" {
+  content  = aws_instance.dbserver.public_ip
+  filename = "config.json"
+}
+
+
+/*
 output "appserver_ip_addr" {
-  value       = aws_instance.appserver.public_ip
-  description = "The public IP address of the main server instance."
+  value = { "ip": aws_instance.appserver.public_ip,
+            "dns": aws_instance.appserver.public_dns,
+            "login": "ssh -L localhost:8888:localhost:8888 -i <pem file> ubuntu@${aws_instance.appserver.public_dns}"}
+  description = "The public IP address of the app server instance."
 }
-
-output "appserver_dns" {
-  value = aws_instance.appserver.public_dns
-  description = "The public dns of the main server instance."
-}
-
-output "webserver_login_details" {
-  value = "ssh -L localhost:8888:localhost:8888 -i <pem file> ubuntu@${aws_instance.webserver.public_ip}"
-}
-
-output "appserver_login_details" {
-  value = "ssh -L localhost:8888:localhost:8888 -i <pem file> ubuntu@${aws_instance.appserver.public_ip}"
-}
+*/
