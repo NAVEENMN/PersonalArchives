8a00905d8e34a981b3ba2666a2b3a9afc281fed1
Naveen Mysore <naveenmysore@Naveens-MacBook-Pro.local>
2019-09-29 14:07:26 -0700

    add_image_data

--- core/dbserver/apis/Image/lambda_function.py
+++ core/dbserver/apis/Image/lambda_function.py
@@ -1,9 +1,10 @@
 import json
 import pymongo
+from bson import json_util, ObjectId
 
-def construct_error_resp(code=400, message="Invalid request"):
-    return { 'statusCode': code, 'body': json.dumps(message)}
-
+def construct_resp(code=400, message="Invalid request"):
+    return { 'statusCode': code, 'body': json.dumps(message, default=json_util.default)}
+    
 db_url = "mongodb+srv://admin:cortexadmin123@cluster0-onaoj.mongodb.net/test?retryWrites=true&w=majority"
 
 class database():
@@ -14,7 +15,7 @@ class database():
     def get_conn(self, db_name=None):
         conn = None
         if db_name not in self.databases:
-            return construct_error_resp(404, "db not found")
+            return construct_resp(404, "db not found")
         else:
             conn = self.client[db_name]
         return conn            
@@ -22,21 +23,24 @@ class database():
 db = database()
 def handle_task(task, payload):
     result = {}
-    if task == "get_loc":
-        db_conn = db.get_conn(db_name="sample_geospatial")
-        collection = db_conn["shipwrecks"]
-        if "query" in payload:
-            result = collection.find_one(payload["query"])
+    if task == "add_image_data":
+        db_conn = db.get_conn(db_name="images")
+        collection = db_conn["metadata"]
+        try:
+            res = collection.insert_one(payload)
+            res = str(res.inserted_id)
+            message = {"status":"ok", "image_id": res}
+            result = construct_resp(code=200, message=message)
+        except Exception as e:
+            result = construct_resp(code=500, message=str(e))
     return result
 
 def lambda_handler(event, context):
     params = event
     if "body" in event:
         params = json.loads(event["body"]) # json
-    if "task" not in params:
-        resp = invalid_error
+    if ("task" not in params) or ("payload" not in params):
+        response = invalid_error
     else:
         response = handle_task(params["task"], params["payload"])
-        response.pop("_id") # object is not json searlize error
-        resp = { 'statusCode': 200, 'body': json.dumps(response)}
-    return resp
+    return response
